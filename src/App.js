b1f53317b96708476e000043e9c0159c37393431
mohammadalimojtahedsoleimani <77240820+mohammadalimojtahedsoleimani@users.noreply.github.com>
2022-01-04 23:19:20 +0330

    main body added and used reuseable components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import Navbar from "./components/navbar_components/Navbar";
 import Banner from "./components/banner_components/Banner";
+import Cards from "./components/cards_components/Cards";
 class App extends Component {
     render() {
         return (
             <div>
                 <Navbar/>
                 <Banner/>
+                <Cards/>
             </div>
         );
     }
