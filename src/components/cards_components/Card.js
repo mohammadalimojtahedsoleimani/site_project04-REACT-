8aaf4b0f17dbc13a94993ec673a76959b213caa3
mohammadalimojtahedsoleimani <77240820+mohammadalimojtahedsoleimani@users.noreply.github.com>
2022-01-15 20:06:30 +0330

    some changes in card ang cards got dynamic and some changes in input box

--- src/components/cards_components/Card.js
+++ src/components/cards_components/Card.js
@@ -1,13 +1,39 @@
 import React, {Component} from 'react';
 import styles from "./Card.module.css";
+import down from "../../images/down.svg"
+import up from "../../images/up.svg"
 class Card extends Component {
+    constructor (props) {
+        super (props);
+        this.state = {
+            counter : 0,
+        }
+    }
+    downHandler = () => {
+        if (this.state.counter >= 1) {
+            this.setState ( ( prevState ) => ( {
+                counter : prevState.counter - 1 ,
+            } ) )
+        }
+    }
+    upHandler = () => {
+        this.setState((prevState)=>({
+            counter : prevState.counter + 1,
+        }))
+    }
     render() {
         const {image,name,cost} = this.props;
         return (
             <div className={styles.container}>
-                <img src={image} alt="phone photo "/>
+                {/*chon khode 0 be sorat defult false shart ro barax dadim ta ejra she*/}
+                <img  src={image} alt="phone photo "/>
                 <h3>{name}</h3>
                 <p>{cost}</p>
+                <div className={styles.counter}>
+                    <img className={this.state.counter ? " " : styles.deactive } src={down} alt="axe" onClick={this.downHandler}/>
+                    <span>{this.state.counter}</span>
+                    <img src={up} alt="axe" onClick={this.upHandler}/>
+                </div>
             </div>
         );
     }
--- src/components/search_components/Search.js
+++ src/components/search_components/Search.js
@@ -1,11 +1,32 @@
-import React, {Component} from 'react';
+import React , { Component } from 'react';
 import styles from "./Search.module.css";
+
 class Search extends Component {
-    render() {
+    constructor ( props ) {
+        super ( props );
+        this.state = {
+            text : "" ,
+        }
+    }
+
+    changeHandler = ( event ) => {
+        console.log ( event.target.value )
+        this.setState ( {
+            text : event.target.value ,
+        } )
+    }
+
+    render () {
         return (
-            <div className={styles.container}>
+            <div className={ styles.container }>
                 <p>search what you want ? </p>
-                <input type="text" placeholder="search ... "/>
+                <div>
+                    <input value={ this.state.text } onChange={ this.changeHandler } type="text"
+                           placeholder="search ... "/>
+                    <br/>
+                    <br/>
+                    <span>{ this.state.text }</span>
+                </div>
             </div>
         );
     }
